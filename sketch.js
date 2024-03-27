8bcbb8eb97762973fe40154af5d4bc4ad04f1149
DavidBatkay <david.batkay@emanuel.ro>
2024-03-27 16:34:13 +0200

    added house, sun and grass

--- sketch.js
+++ sketch.js
@@ -1,7 +1,32 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(displayWidth, displayHeight);
+  background(220);
+  drawHouse(); 
+}
+
+function drawHouse() {
+  fill(0, 255, 0); 
+  rect(0, 500, width, 200); 
+
+  fill(255, 235, 139); 
+  rect(150, 250, 100, 100); 
+
+  
+  fill(255, 0, 0); 
+  triangle(150, 250, 250, 250, 200, 200); 
+
+  
+  fill(139, 69, 19); 
+  rect(180, 300, 40, 50); 
+
+  
+  fill(255); 
+  rect(165, 270, 20, 20); 
+  rect(215, 270, 20, 20); 
 }
 
 function draw() {
-  background(220);
+
+  fill(255, 204, 0); 
+  ellipse(50, 50, 80, 80); 
 }
